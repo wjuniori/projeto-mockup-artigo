aa3bf94b99244cf4310afd6c242504f0d4edfc5f
wjuniori <wjuniori.si@gmail.com>
2018-12-12 19:40:12 -0300

    1. Concluído o PROJETO: Do mockup ao artigo
    => gulpfile.js, package-lock.json e package.json
    
    1. Desinstalado a dependência "imageMin"
    2. Excluído a task "img"
    3. Downgrade da versão do "gulp" de 4.0.0 para 3.9.1
    4. Retirado o ^ (acento circunflexo) da versão dos packages, exceto do "gulp"
    
    => index.html
    
    1. Alterado a estrutura html do body

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var imageMin = require('gulp-imagemin');
 var htmlMin = require('gulp-htmlmin');
 var notify = require('gulp-notify');
 
-var imgFiles = [
-  './public/assets/img/**/*.jpg',
-  './public/assets/img/**/*.jpeg',
-  './public/assets/img/**/*.gif',
-  './public/assets/img/**/*.png',
-  './public/assets/img/**/*.svg',
-  '!./public/assets/img/originals/**/*'
-];
 var htmlFile = ['./public/assets/*.html'];
 
 // Minify HTML
@@ -28,15 +19,8 @@ gulp.task('html:minify', function() {
 // HTML
 gulp.task('html', ['html:minify']);
 
-// Minify Image
-gulp.task('img', function() {
-  return gulp.src(imgFiles)
-    .pipe(imageMin())
-    .pipe(gulp.dest('./public/dist/img/'));
-});
-
 // Default task
-gulp.task('default', ['html', 'img']);
+gulp.task('default', ['html']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -48,7 +32,6 @@ gulp.task('browserSync', function() {
 });
 
 //Dev task
-gulp.task('dev', ['html', 'img', 'browserSync'], function() {
-  gulp.watch(imgFiles, ['img']);
+gulp.task('dev', ['html', 'browserSync'], function() {
   gulp.watch(htmlFile, ['html']);
 });
